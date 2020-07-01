dce0894211fbda88aa8af5c2e23945ae91a02d8c
balibabu <yibie_jingnian@163.com>
2020-07-01 17:28:29 +0800

    download iconfont by css link

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+// 将字体文件过滤出来
+const filterUrl = (str) =>
+  str
+    .match(/url\('\/\/at\.alicdn\.com[\/a-z_\d=?.#]+'\)/g)
+    .filter((x) => /^((?!iefix).)*$/.test(x));
+
 const download = (url, dest) => {
-  const file = fs.createWriteStream(path.join(process.cwd(), "assets"));
-  const req = http.get(`http://at.alicdn.com/t/${url}.css`, function (res) {
+  const file = fs.createWriteStream(path.join(process.cwd(), dest));
+  const req = http.get(url, function (res) {
     let rawData = "";
     res.on("data", (chunk) => {
       rawData += chunk;
     });
     res.on("end", () => {
       try {
-        console.log(rawData);
+        if (dest === "assets.css") {
+          const fontUrls = filterUrl(rawData);
+          console.log(fontUrls);
+          const nextUrls = fontUrls.map((x) => ({
+            url: `http:${x.slice(5, -2)}`,
+            ext: x.match(/\.([eotwfsvg]{3,4})(?=\?)/g)[0], //扩展名
+          }));
+          nextUrls.forEach((x) => {
+            download(x.url, `iconfont${x.ext}`);
+          });
+        }
       } catch (e) {
         console.error(e.message);
       }
@@ -20,4 +36,6 @@ const download = (url, dest) => {
   });
 };
 
-download("font_1463774_58ziz8lisc2");
+const cssKey = "font_1463774_58ziz8lisc2";
+
+download(`http://at.alicdn.com/t/${cssKey}.css`, "assets.css");
