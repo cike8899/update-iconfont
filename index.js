11ffaf2c2ff6796ffa0e099d997d2b4fe53d1a81
balibabu <yibie_jingnian@163.com>
2020-07-01 18:35:38 +0800

    repalce font url with local url

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const path = require("path");
 
 // 将字体文件过滤出来
 const filterUrl = (str) =>
-  str
-    .match(/url\('\/\/at\.alicdn\.com[\/a-z_\d=?.#]+'\)/g)
-    .filter((x) => /^((?!iefix).)*$/.test(x));
-
+  str.match(/url\('\/\/at\.alicdn\.com[\/a-z_\d=?.#]+'\)/g);
 const download = (url, dest) => {
   const file = fs.createWriteStream(path.join(process.cwd(), dest));
   const req = http.get(url, function (res) {
@@ -20,19 +17,31 @@ const download = (url, dest) => {
         if (dest.endsWith("iconfont.css")) {
           const fontUrls = filterUrl(rawData);
           console.log(fontUrls);
-          const nextUrls = fontUrls.map((x) => ({
-            url: `http:${x.slice(5, -2)}`,
-            ext: x.match(/\.([eotwfsvg]{3,4})(?=\?)/g)[0], //扩展名
-          }));
+          const nextUrls = fontUrls
+            .filter((x) => /^((?!iefix).)*$/.test(x))
+            .map((x) => ({
+              url: `http:${x.slice(5, -2)}`,
+              ext: x.match(/\.([eotwfsvg]{3,4})(?=\?)/g)[0], //扩展名
+            }));
+
           nextUrls.forEach((x) => {
             download(x.url, `iconfont${x.ext}`);
           });
+
+          // 替换字体路径为本地
+          const nextData = rawData.replace(
+            /(\/\/at\.alicdn\.com)([\S]+)(?=\.)/g,
+            "iconfont"
+          );
+          fs.writeFileSync(path.join(process.cwd(), dest), nextData);
         }
       } catch (e) {
         console.error(e.message);
       }
     });
-    res.pipe(file);
+    if (!dest.endsWith("iconfont.css")) {
+      res.pipe(file);
+    }
   });
 };
 
